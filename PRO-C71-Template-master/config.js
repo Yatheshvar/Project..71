f60b68a3a29a1aecd2f308a870b399934961ceea
Yatheshvar <64597185+Yatheshvar@users.noreply.github.com>
2022-07-31 13:55:41 +0530

    Update config.js

--- PRO-C71-Template-master/config.js
+++ PRO-C71-Template-master/config.js
@@ -2,7 +2,14 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 //Paste your firebaseConfig here
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAsmzvtGAmNS4l_wrywhw7Be7w3ABorO6E",
+  authDomain: "e-ride-e6f31.firebaseapp.com",
+  projectId: "e-ride-e6f31",
+  storageBucket: "e-ride-e6f31.appspot.com",
+  messagingSenderId: "730538285419",
+  appId: "1:730538285419:web:c1f3b7a0748b6831ff542e"
+};
 //
 firebase.initializeApp(firebaseConfig);
 
